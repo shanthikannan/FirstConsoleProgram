3a6d3ab118c9e66ac08ec53fe65f91a9c7604170
shanthikannan.d <shanthikannan.d@gmail.com>
2017-09-23 18:18:24 -0400

    Basic program execution with a simple function with no input/output

--- FirstConsoleProgram/Program.cs
+++ FirstConsoleProgram/Program.cs
@@ -10,6 +10,38 @@ namespace FirstConsoleProgram
     {
         static void Main(string[] args)
         {
+            String xyz;
+            printWelcomeMessage();
+
+            int a;
+            Console.WriteLine("Enter a number!");
+            xyz = Console.ReadLine();
+            Int32.TryParse(xyz, out a); //Convert the string xyz to the integer a
+            
+            int b;
+            Console.WriteLine("Enter a number!");
+            xyz = Console.ReadLine();
+            Int32.TryParse(xyz, out b); //Convert the string xyz to the integer b
+
+            int c;
+            Console.WriteLine("Enter a number!");
+            xyz = Console.ReadLine();
+            Int32.TryParse(xyz, out c); //Convert the string xyz to the integer c
+            
+            int d;
+            Console.WriteLine("Enter a number!");
+            xyz = Console.ReadLine();
+            Int32.TryParse(xyz, out d); //Convert the string xyz to the integer d
+
+            Console.WriteLine("The result is ");
+            Console.WriteLine(a + b + c + d);
+            Console.ReadKey();
+        }
+
+        static void printWelcomeMessage()
+        {
+            Console.WriteLine("This is Shanthi!");
+            Console.WriteLine("Welcome to my first program!");
         }
     }
 }
