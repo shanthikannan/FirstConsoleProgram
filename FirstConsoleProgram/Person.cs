20782a84cb076c5a7c79ee88e33c8a0c6af85d66
shanthikannan.d <shanthikannan.d@gmail.com>
2017-09-25 23:23:24 -0400

    Class and Object

---
+++ FirstConsoleProgram/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstConsoleProgram
+{
+    enum Sex { MALE, FEMALE};
+    class Person
+    {
+        public string firstName;
+        public string lastName;
+        public int age;
+        public Sex sex;
+        private int points = 0;
+
+        public void addPoint()
+        {
+            points++;
+        }
+
+        public int getPoints()
+        {
+            return points;
+        }
+    }
+}
--- FirstConsoleProgram/Program.cs
+++ FirstConsoleProgram/Program.cs
@@ -10,60 +10,42 @@ namespace FirstConsoleProgram
     {
         static void Main(string[] args)
         {
-            const int SIZE = 4;
-            printWelcomeMessage();
+            Person shan = new Person();
+            shan.firstName = "Shathi";
+            shan.lastName = "K";
+            shan.age = 37;
+            shan.sex = Sex.FEMALE;
 
-            int result = 0;
-            int counter = 0;
-            int[] a = new int[SIZE];
+            Person kannan = new Person();
+            kannan.firstName = "Kannan";
+            kannan.lastName = "l";
+            kannan.age = 39;
+            kannan.sex = Sex.MALE;
 
+            Person deekshu = new Person();
+
+            String xyz;
             do
             {
-                a[counter] = readIntegerFromConsole(); // call the function and store the return value to a
-                counter = counter + 1;
-            } while (counter < a.Length);
-
-
-            Console.WriteLine("Here is your result");
-            foreach(int element in a)
-            //for (counter = 0; counter < a.Length; counter++)
-            {
-                result = result + element;
-                //result = result + a[counter];// result += a
-                Console.WriteLine(element + " + ");
-                //Console.WriteLine(a[counter] + " + ");
-            }
-            Console.WriteLine("------");
-            Console.WriteLine(result);
-            Console.WriteLine("------");
-
-            Console.ReadKey();
-        }
-
-        static int readIntegerFromConsole()
-        {
-            int x;
-            Console.WriteLine("Enter a number!");// write the line in the console
-            String xyz = Console.ReadLine();// Read the line from console and store it to xyz
-           
-            Int32.TryParse(xyz, out x); //Convert the string xyz to the integer x
-           /* if (x == 0){
-                Console.WriteLine("Enter a number instead of a string!");// write the line in the console
                 xyz = Console.ReadLine();
-                Int32.TryParse(xyz, out x);
-            }
-            else
-            {
-                Console.WriteLine("Thank you");
-            }
-            */
-            return x; // return the value x to the caller
-        }
+                if (xyz == "ABC")
+                {
+                    kannan.addPoint();
+                }
+                else if (xyz == "XYZ")
+                {
+                    shan.addPoint();
+                }
+            } while (xyz != "z");
 
-        static void printWelcomeMessage()
-        {
-            Console.WriteLine("This is Shanthi!");
-            Console.WriteLine("Welcome to my first program!");
-        }
+            Console.WriteLine("THe Winner is");
+            if (kannan.getPoints() > shan.getPoints())
+            {
+                Console.WriteLine("KANNAN !!!");
+            }else
+            {
+                Console.WriteLine("SHAN !!!");
+            }
+        }  
     }
 }
